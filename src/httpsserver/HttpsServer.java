3e2e04aabe780f53e3da545c3151d6828815388a
Marcel Teixeira <marceltex@gmail.com>
2016-07-04 13:33:58 +0200

    Initial Commit

---
+++ src/httpsserver/HttpsServer.java
@@ -0,0 +1,17 @@
+package httpsserver;
+
+/**
+ * A simple HTTPS server used to provide an SSL connection to an Android app.
+ * 
+ * @author Marcel Teixeira
+ * @version 1.0
+ */
+public class HttpsServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    } 
+}
